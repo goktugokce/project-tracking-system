bce866015cdc2a465b16925c788aee53748c6611
AydinAlptug <yusufaydinceng@gmail.com>
2022-01-05 17:10:42 +0300

    sequelize setup for database

---
+++ backend/migrations/20220105122401-create-course.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      courseName: {
+        type: Sequelize.STRING
+      },
+      lecturerId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Lecturers',
+          key: 'id',
+          as: 'lecturerId'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Courses');
+  }
+};
